f94de3d7c408dcbecff46384e3f0b248a8f513ce
nevermore <miloom@ttu.ee>
2022-10-04 17:30:41 +0300

    Initial Commit!

---
+++ src/main/java/ee/taltech/iti0302/robotiklubi/Iti0302RobotiklubiApplication.java
@@ -0,0 +1,13 @@
+package ee.taltech.iti0302.robotiklubi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Iti0302RobotiklubiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Iti0302RobotiklubiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ee/taltech/iti0302/robotiklubi/Iti0302RobotiklubiApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.taltech.iti0302.robotiklubi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Iti0302RobotiklubiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
