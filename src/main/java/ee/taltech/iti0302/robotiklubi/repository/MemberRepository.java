6b5e8bef9ccb59cbb4c8dc888f244ddca07daf88
saplee <saplee@ttu.ee>
2022-10-06 17:34:41 +0300

    Member class repository

---
+++ src/main/java/ee/taltech/iti0302/robotiklubi/repository/MemberRepository.java
@@ -0,0 +1,7 @@
+package ee.taltech.iti0302.robotiklubi.repository;
+
+import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
+
+public interface MemberRepository extends JpaRepositoryImplementation<Member, Long> {
+
+}
