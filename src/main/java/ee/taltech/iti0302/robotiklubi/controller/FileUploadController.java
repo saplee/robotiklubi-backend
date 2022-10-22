07c4a77f1d5f9b140d0297e848ce0f48f3aac024
Rainer Viirlaid <raviir@ttu.ee>
2022-10-23 00:33:03 +0300

    Uploaded files are now saved to the 'uploads' directory. Added NotNull tags to fix RequiredArgsConstructor tag.

--- src/main/java/ee/taltech/iti0302/robotiklubi/controller/FileUploadController.java
+++ src/main/java/ee/taltech/iti0302/robotiklubi/controller/FileUploadController.java
@@ -1,5 +1,8 @@
 package ee.taltech.iti0302.robotiklubi.controller;
 
+import com.sun.istack.NotNull;
+import ee.taltech.iti0302.robotiklubi.service.FileUploadService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,14 +10,15 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
+@RequiredArgsConstructor
 @RestController
 public class FileUploadController {
 
-    @PostMapping("/upload")
-    public String handleFileUpload(@RequestParam("file") MultipartFile file) throws IOException {
-        String fileName  = file.getOriginalFilename();
-        int fileLength = file.getBytes().length;
-        return "File uploaded successfully, " + "file name is "+ fileName + " and length is " + fileLength;
-    }
+    @NotNull
+    FileUploadService fileUploadService;
 
+    @PostMapping("/upload")
+    public String uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+        return fileUploadService.saveToDisk(file);
+    }
 }
---
+++ src/main/java/ee/taltech/iti0302/robotiklubi/service/FileUploadService.java
@@ -0,0 +1,20 @@
+package ee.taltech.iti0302.robotiklubi.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+@Service
+public class FileUploadService {
+
+    public String saveToDisk(MultipartFile file) throws IOException {
+        File localFile = new File(Paths.get("").toAbsolutePath() + "\\uploads\\" + file.getOriginalFilename());
+        String fileName  = file.getOriginalFilename();
+        int fileLength = file.getBytes().length;
+        file.transferTo(localFile);
+        return "File uploaded successfully, " + "file name is "+ fileName + " and length is " + fileLength + " bytes.";
+    }
+}
