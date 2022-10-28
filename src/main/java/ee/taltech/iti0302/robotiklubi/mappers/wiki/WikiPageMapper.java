e2bc0e8076fbb8a905306cbf6ba4a77e9a69fc90
Rainer Viirlaid <raviir@ttu.ee>
2022-10-28 14:37:43 +0300

    Fixed wrong dependency order.

--- src/main/java/ee/taltech/iti0302/robotiklubi/mappers/wiki/WikiPageMapper.java
+++ src/main/java/ee/taltech/iti0302/robotiklubi/mappers/wiki/WikiPageMapper.java
@@ -3,13 +3,10 @@ package ee.taltech.iti0302.robotiklubi.mappers.wiki;
 import ee.taltech.iti0302.robotiklubi.dto.wiki.WikiPageDto;
 import ee.taltech.iti0302.robotiklubi.repository.WikiPage;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface WikiPageMapper {
 
-    @Mapping(source = "title", target = "title")
-    @Mapping(source = "content", target = "content")
     WikiPageDto toDto(WikiPage wikiPage);
 }
