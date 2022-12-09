5fc202c920f2fa33718b354f8f02ba8f8cb3f98c
Rainer Viirlaid <raviir@ttu.ee>
2022-12-09 18:07:38 +0200

    Fixed permissions and used them for wiki and tag endpoints.

--- src/main/java/ee/taltech/iti0302/robotiklubi/controller/user/UserController.java
+++ src/main/java/ee/taltech/iti0302/robotiklubi/controller/user/UserController.java
@@ -4,11 +4,13 @@ import ee.taltech.iti0302.robotiklubi.dto.user.*;
 import ee.taltech.iti0302.robotiklubi.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.List;
 
 @Slf4j
@@ -17,6 +19,12 @@ import java.util.List;
 public class UserController {
 
     private final UserService userService;
+
+    @PreAuthorize("hasAuthority('USER')")
+    @GetMapping("/users/")
+    public UserDto getUserData(Principal principal) {
+        return userService.getUserInfo(principal.getName());
+    }
 
     @GetMapping("/users/members")
     public List<UserDto> getAllRegularClubMembers() {
