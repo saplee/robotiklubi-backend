17a4a77111a62b0cfd6af8ff85dd07c813ce989a
saplee <saplee@ttu.ee>
2022-12-16 13:40:00 +0200

    tests

--- src/test/java/ee/taltech/iti0302/robotiklubi/test/integration/controller/WikiTagRelationControllerTest.java
+++ src/test/java/ee/taltech/iti0302/robotiklubi/test/integration/controller/WikiTagRelationControllerTest.java
@@ -1,6 +1,8 @@
 package ee.taltech.iti0302.robotiklubi.test.integration.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import ee.taltech.iti0302.robotiklubi.dto.wiki.TagDto;
+import ee.taltech.iti0302.robotiklubi.dto.wiki.TagListDto;
 import ee.taltech.iti0302.robotiklubi.dto.wiki.WikiPageDto;
 
 import ee.taltech.iti0302.robotiklubi.dto.wiki.WikiSearchCriteria;
@@ -15,6 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
@@ -22,9 +25,9 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.util.MultiValueMap;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-
 
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -49,6 +52,28 @@ class WikiTagRelationControllerTest extends AbstractIntegrationTest {
         context.setAuthentication(new UsernamePasswordAuthenticationToken(1, null, List.of(new SimpleGrantedAuthority("MEMBER"))));
     }
 
+    @Test
+    void tagRelation() throws Exception {
+        mvc.perform(post("/tags/relation/create?pageId=12345&tagId=66")).andExpect(status().isOk());
+    }
 
+    @Test
+    void tagRelationMAny() throws Exception {
+        TagListDto tagListDto = new TagListDto();
+        TagDto tagDto = TagDto.builder().tag("Tank").id(123L).build();
+        TagDto tagDto2 = TagDto.builder().tag("Plane").id(1234L).build();
+        List<TagDto> tagDtoList = new ArrayList<>(List.of(tagDto2, tagDto));
+        tagListDto.setTags(tagDtoList);
+        mvc.perform(post("/tags/relation/create/many?pageId=12345").contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(tagListDto))).andExpect(status().isOk());
+    }
 
+    @Test
+    void deleteTag() throws Exception {
+        TagListDto tagListDto = new TagListDto();
+        TagDto tagDto = TagDto.builder().tag("Tank").id(123L).build();
+        TagDto tagDto2 = TagDto.builder().tag("Plane").id(1234L).build();
+        List<TagDto> tagDtoList = new ArrayList<>(List.of(tagDto2, tagDto));
+        tagListDto.setTags(tagDtoList);
+        mvc.perform(delete("/tags/relation/delete?pageId=12345&tagId=3")).andExpect(status().isOk());
+    }
 }
